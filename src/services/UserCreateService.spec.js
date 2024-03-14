bffda31d3273669e7c617edead2af53ef240a75c
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-03-14 15:46:16 -0300

    test to create user with same email

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,18 +1,52 @@
 const UserCreateService = require("./UserCreateService")
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require("../utils/AppError")
 
-it("user should be create", async () => {
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123",
-  }
+describe("UserCreateService", () => {
+  let userRepositoryInMemory = null
+  let userCreateService = null
 
-  const userRepositoryInMemory = new UserRepositoryInMemory()
-  const userCreateService = new UserCreateService(userRepositoryInMemory)
-  const userCreated = await userCreateService.execute(user)
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory()
+    userCreateService = new UserCreateService(userRepositoryInMemory)
+  })
 
-  /* console.log(userCreated) */
+  it("user should be create", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123",
+    }
 
-  expect(userCreated).toHaveProperty("id")
+    const userCreated = await userCreateService.execute(user)
+    /* console.log(userCreated) */
+    expect(userCreated).toHaveProperty("id")
+  })
+
+  it("user not should be created with exists email", async () => {
+    const user1 = {
+      name: "User Test 1",
+      email: "user@test.com",
+      password: "123",
+    }
+
+    const user2 = {
+      name: "User Test 2",
+      email: "user@test.com",
+      password: "456",
+    }
+
+    await userCreateService.execute(user1)
+    await expect(userCreateService.execute(user2)).rejects.toEqual(
+      new AppError("Este e-mail já está em uso.")
+    )
+  })
 })
+
+/* Apenas para teste de grupos de test, não é o ideal ter 2 describes/grupos no mesmo arquivo de test
+describe("NotesCreateService", () => {
+  it("create notes", () => {
+    expect(1).toBe(1)
+  })
+})
+*/
